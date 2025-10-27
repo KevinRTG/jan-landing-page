5c8a2e47923399901fd1adb1667ddb6ddc33836e
Kevin <kevinsuyadi666@gmail.com>
2025-10-28 01:44:12 +0700

    checkpoint 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
+'use client';
+
+import { useEffect } from 'react';
 import Hero from '../components/Hero';
 import IntroSection from '../components/IntroSection';
 import FeaturedServices from '../components/FeaturedServices';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function Home() {
+  useEffect(() => {
+    AOS.init({ duration: 800, once: true });
+  }, []);
+
   return (
     <div className="min-h-screen bg-white">
-      <Hero />
-      <IntroSection />
-      <FeaturedServices />
-      
+      <section data-aos="fade-up">
+        <Hero />
+      </section>
+      <section data-aos="fade-up" data-aos-delay="100">
+        <IntroSection />
+      </section>
+      <section data-aos="fade-up" data-aos-delay="200">
+        <FeaturedServices />
+      </section>
     </div>
   );
 }
