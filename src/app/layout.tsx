6184c80c1fb7f0dd697600806f348fe1e3e7625e
Kevin <kevinsuyadi666@gmail.com>
2025-11-13 16:59:20 +0700

    verify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
   title: 'JAN Nusantara',
   description: 'Solusi konstruksi dan pengadaan terpercaya dari JAN Nusantara.',
   keywords: ['JAN Nusantara', 'konstruksi', 'pengadaan', 'telekomunikasi', 'furniture laboratorium'],
+  other: {
+    'google-site-verification': 'qesU1wefPe8EnnbllPM-i7k-wXIs_Fn0DAnG2H65BhQ',
+  },
 };
 
 export default function RootLayout({
