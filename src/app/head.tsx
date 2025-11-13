0ec21a58bc419f1336d7cbb41d03526953ed33e4
Kevin <kevinsuyadi666@gmail.com>
2025-11-13 16:52:56 +0700

    verrify

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -2,7 +2,7 @@ export default function Head() {
   return (
     <>
       <link rel="icon" href="/favicon.ico" type="image/png" />
-      <meta name="google-site-verification" content="googlee1721341420301b6.html" />
+      <meta name="google-site-verification" content="qesU1wefPe8EnnbllPM-i7k-wXIs_Fn0DAnG2H65BhQ" />
       <script type="application/ld+json">
         {JSON.stringify({
           "@context": "https://schema.org",
